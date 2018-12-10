2dfc8d699164d6ce211b15d43579a722a422c0d6
aguiot-- <aguiot--@student.42.fr>
2018-12-10 13:02:24 +0100

    Fix topleft (bitwise)

--- topleft.c
+++ topleft.c
@@ -1,5 +1,24 @@
 #include "fillit.h"
 
+char	*bits_to_chars(unsigned short octet)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0b1000000000000000;
+	j = 15;
+	str = ft_strnew(16);
+	while (octet >= 0 && i)
+	{
+		str[j] = (octet / i) ? '#': '.';
+		(octet / i) ? octet -= i : 0;
+		i /= 2;
+		--j;
+	}
+	return (str);
+}
+
 unsigned short	topleft(unsigned short bits)
 {
 	int		i;
@@ -12,5 +31,11 @@ unsigned short	topleft(unsigned short bits)
 		i++;
 	}
 	bits = bits >> (i - 1);
+	if (check_tetrimino(bits_to_chars(bits)) == 1)
+	{
+		bits = bits << 1;
+		if (check_tetrimino(bits_to_chars(bits)) == 1)
+			bits = bits << 1;
+	}
 	return (bits);
 }
