9cacd673672e15dda41940cce533e8d0b1086ee7
achoquel <adiklank@gmail.com>
2018-12-10 13:41:56 +0100

    Swap du ft_strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -3,79 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: achoquel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: aguiot-- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/09 11:21:00 by achoquel          #+#    #+#             */
-/*   Updated: 2018/11/23 12:23:10 by achoquel         ###   ########.fr       */
+/*   Created: 2018/12/10 11:57:24 by aguiot--          #+#    #+#             */
+/*   Updated: 2018/12/10 11:57:47 by achoquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "includes/libft.h"
 
-static int		ft_is_blank(char c)
+char	*ft_strtrim(char const *s)
 {
-	return (c == ' ' || c == '\t' || c == '\n');
-}
-
-static int		ft_start(char const *s)
-{
-	int	st;
-
-	st = 0;
-	if (!ft_is_blank(s[st]))
-		return (st);
-	else
-	{
-		while (ft_is_blank(s[st]))
-		{
-			st++;
-		}
-	}
-	return (st);
-}
-
-static int		ft_stop(char const *s)
-{
-	int	sp;
-
-	sp = ft_strlen((char *)s) - 1;
-	if (!ft_is_blank(s[sp]))
-		return (sp);
-	else
-	{
-		while (ft_is_blank(s[sp]))
-		{
-			sp--;
-		}
-	}
-	return (sp);
-}
-
-char			*ft_strtrim(char const *s)
-{
-	int		start;
-	int		stop;
-	char	*f;
-
-	if (s)
-	{
-		start = ft_start(s);
-		stop = ft_stop(s);
-		if ((stop - start + 1) < 0)
-		{
-			if ((f = (char *)malloc(sizeof(char) * 1)) == NULL)
-				return (NULL);
-			f[0] = '\0';
-		}
-		else
-		{
-			if ((f = (char *)malloc(sizeof(char) * (stop - start + 1))) == NULL)
-				return (NULL);
-			f = ft_strsub(s, start, (stop - start + 1));
-			f[stop - start + 1] = '\0';
-		}
-		return (f);
-	}
-	return (NULL);
+	char    *trim;
+	int        i;
+	int        j;
+	int        k;
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s[i] && (s[i] == '.'))
+		++i;
+	j = ft_strlen(s) - 1;
+	while (s[j] && (s[j] == '.'))
+		--j;
+	if (j == -1)
+		i = 0;
+	if ((trim = ft_strnew(j - i + 1)) == NULL)
+		return (NULL);
+	k = 0;
+	while (i <= j)
+		trim[k++] = s[i++];
+	return (trim);
 }
